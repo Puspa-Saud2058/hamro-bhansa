c067c8a9324a3b9a061c93f97f073259241b533d
Puspa-Saud2058 <puspasaud101@gmail.com>
2023-11-20 08:54:58 +0545

    add validation for email repeatation

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,22 @@
 const express = require('express')
+const cors=require('cors')
 const connection=require('./db/connection')
 const customer=require('./models/customer')
 const app = express()
 app.use(express.json())
+app.use(cors())
 const port = 4000
 connection()
 app.post('/register', async(req, res) => {
-  await customer.create(req.body)
-})
+  const customerExists=await customer.findOne({email:req.body.email})
+  if(customerExists){
+    res.status(409).json({msg:'Email already exist'})
+  }
+  else{
+   const data= await customer.create(req.body)
+   if(data)res.json({msg:'customer register'})
+  }
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
