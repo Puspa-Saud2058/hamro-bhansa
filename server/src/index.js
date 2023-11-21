5f49351eba1934f625c12b3d8f3ce926eef4b109
Puspa-Saud2058 <puspasaud101@gmail.com>
2023-11-21 08:06:08 +0545

    add password encryption hash function

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,37 @@
 const express = require('express')
-const cors=require('cors')
-const connection=require('./db/connection')
-const customer=require('./models/customer')
+const cors = require('cors')
+const connection = require('./db/connection')
+const customer = require('./models/customer')
+
 const app = express()
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
 app.use(express.json())
 app.use(cors())
+
 const port = 4000
+
 connection()
-app.post('/register', async(req, res) => {
-  const customerExists=await customer.findOne({email:req.body.email})
-  if(customerExists){
-    res.status(409).json({msg:'Email already exist'})
+
+app.post('/register', async (req, res) => {
+  try {
+    const hashPassword=await bcrypt.hash(req.body.password, saltRounds)
+    console.log(hashPassword)
+    const customerExists = await customer.findOne({ email: req.body.email })
+
+    if (customerExists) {
+      res.status(409).json({ msg: 'Email already exists' })
+    } else {
+      const data = await customer.create(req.body)
+
+      if (data) res.json({ msg: 'Customer registered! Please login' })
+    }
+  } catch (err) {
+    console.log(err)
   }
-  else{
-   const data= await customer.create(req.body)
-   if(data)res.json({msg:'customer register'})
-  }
-  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
