ebdb53d4bf94a8f6a684c0277922ba14c4ac4d16
Puspa-Saud2058 <puspasaud101@gmail.com>
2023-11-25 20:45:50 +0545

    made login page validation

--- server/src/index.js
+++ server/src/index.js
@@ -1,56 +1,67 @@
-const express = require('express')
-const cors = require('cors')
-const connection = require('./db/connection')
-const customer = require('./models/customer')
-
-const app = express()
+// Importing Dependencies
+const express = require('express');
+const cors = require('cors');
+const connection = require('./db/connection');
+const customer = require('./models/customer');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+// Creating an Express App
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
+// Setting up the Port
+const port = 4000;
 
-const port = 4000
+// Establishing Database Connection
+connection();
 
-connection()
-
+// Registration Endpoint
 app.post('/register', async (req, res) => {
   try {
-    //generate a hash password
-    const hashPassword=await bcrypt.hash(req.body.password, saltRounds)
-       req.body.password=hashPassword 
-           //check if user/email/phoneNumber doesnt already exist
-        const customerExists = await customer.findOne({ email: req.body.email })
+    // Generate a hash password
+    const hashPassword = await bcrypt.hash(req.body.password, saltRounds);
+    req.body.password = hashPassword;
+
+    // Check if user/email/phoneNumber doesn't already exist
+    const customerExists = await customer.findOne({ email: req.body.email });
 
     if (customerExists) {
-      res.status(409).json({ msg: 'Email already exists' })
+      res.status(409).json({ msg: 'Email already exists' });
     } else {
-      //create new user with hash password
-            const data = await customer.create(req.body)
+      // Create a new user with a hashed password
+      const data = await customer.create(req.body);
 
-      if (data) res.json({ msg: 'Customer registered! Please login' })
+      if (data) res.json({ msg: 'Customer registered! Please login' });
     }
   } catch (err) {
-    console.log(err)
+    // Handle errors
+    console.log(err);
   }
-})
-app.post('/login',async(req,res)=>{
-  const customerDetails = await customer.findOne({ email: req.body.email })
-  if(!customerDetails){
-    res.status(404).json({msg:'Invalid Credentials '})
-  }else{
-    console.log(customerDetails.password,req.body.password)
-   const isMatched=await bcrypt.compare(req.body.password,customerDetails.password)
-  if(isMatched){
-    res.status(200).json({msg:'Login success'})
-  }
-  else{
-    res.status(404).json({msg:'Incorrect match'})
-  }
-  }
-})
+});
 
+// Login Endpoint
+app.post('/login', async (req, res) => {
+  const customerDetails = await customer.findOne({ email: req.body.email });
+  if (!customerDetails) {
+    res.status(404).json({ msg: 'Invalid Credentials ' });
+  } else {
+    // Compare hashed password
+    const isMatched = await bcrypt.compare(
+      req.body.password,
+      customerDetails.password
+    );
+
+    if (isMatched) {
+      res.status(200).json({ msg: 'Login success' });
+    } else {
+      res.status(404).json({ msg: 'Incorrect match' });
+    }
+  }
+});
+
+// Start the Express App
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
