cbac7ace4dab44704734c68f03b39dc042da9df9
Puspa-Saud2058 <puspasaud101@gmail.com>
2023-11-29 06:43:25 +0545

    still unable to fix the error

--- server/src/routes/product.js
+++ server/src/routes/product.js
@@ -3,10 +3,10 @@ const router=express.Router()
 const  Product= require('../models/product');
 router.use(express.json());
 
-router.get('/product', async (req, res) => {
+router.post('/product', async (req, res) => {
    const data= await Product.find()
     if(data){
-      res.json({productList:data});
+      res.json({msg:`${req.body.productName} has been created`});
     }
 })
     
